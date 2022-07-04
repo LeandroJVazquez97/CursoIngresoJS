af77d1ed8ac2cbce8a71fe34c1c0487e0df232cf
Leandro <leandrojvazquez97@gmail.com>
2022-07-04 12:30:59 -0300

    Ejercicio 04 incompleto

--- 1-EntradaSalida/jsEntradaSalida-03.js
+++ 1-EntradaSalida/jsEntradaSalida-03.js
@@ -1,12 +1,34 @@
 /*
+Vazquez, Leandro Javier
+DIV E
+Ejercicio 03 E/S
+
 Debemos lograr tomar un dato por 'ID'
 y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	var nombreIngresado;
-	nombreIngresado=txtIdNombre.value;
-	alert("ok");
+	// DECLARAR VARIABLES
+	let nombreIngresado;
 
+	// TOMAR DATOS
+	nombreIngresado = document.getElementById(txtIdNombre).value;
+	
+	// MOSTRAR DATOS O INFORMACIÓN
+	alert(nombreIngresado);
 }
 
 
+/*
+
+-INGRESAMOS DATOS
+
+
+-PROCESAMOS DATOS
+-MOSTRAMOS DATOS
+
+
+
+var nombreIngresado;
+	nombreIngresado=txtIdNombre.value;
+	alert("ok");
+	*/
--- 1-EntradaSalida/jsEntradaSalida-04.js
+++ 1-EntradaSalida/jsEntradaSalida-04.js
@@ -1,9 +1,19 @@
 /* 
+	Vazquez, Leandro Javier
+	DIV E
+	EJERCICIO 04 E/S DE DATOS
+
 Debemos lograr tomar un dato por 'PROMPT' 
 y lo muestro por 'ID' al presionar el botón 'mostrar'
 */
 function mostrar()
 {
+	let nombreIngresado;
+
+	nombreIngresado = prompt("Ingresar el nombre");
+
+	
+
 	alert("ok");
 }
 
