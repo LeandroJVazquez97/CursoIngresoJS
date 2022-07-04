6886c909c257330ed2f558ba6667b7d37a56110f
Leandro <leandrojvazquez97@gmail.com>
2022-07-04 12:03:55 -0300

    Se realizo el ejercicio 2

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -1,8 +1,28 @@
 /*
+Vazquez, Leandro Javier
+DIV E
+Ejercicio 2 E/S
+
 Debemos lograr tomar un nombre con 'prompt' 
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
+
 function mostrar()
 {
-	alert("ok");
+	// Declaro variable 
+	let nombre;
+
+	// Tomar datos
+	nombre = prompt("Introduzca su nombre");
+
+	// Mostrar información
+	alert("Su nombre es:")
+	
+	alert(nombre);
 }
 
+/*
+-vamos a recibir datos.
+-vamos a procesar datos.
+-vamos a mostrar informacion.
+	alert("mensaje")
+*/
