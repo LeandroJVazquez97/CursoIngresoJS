99030a25fd49f4d94d99a66c188cdce3b8c5f2d1
Leandro <leandrojvazquez97@gmail.com>
2022-07-05 11:41:22 -0300

    Realizamos ejercicio 08

--- 1-EntradaSalida/jsEntradaSalida-08.js
+++ 1-EntradaSalida/jsEntradaSalida-08.js
@@ -1,9 +1,24 @@
 /*
+	VAZQUEZ, LEANDRO JAVIER
+	DIV E
+	EJERCICIO 08 E/S
+
 Debemos lograr tomar Los numeros por ID ,
 transformarlos a enteros (parseInt),realizar la operación correcta y 
 mostrar el resto entre el dividendo y el divisor.
 ej.: "El resto es 0 ."*/
 function SacarResto()
 {
-	alert("ok");
+	// CREAMOS VARIABLES PARA EL DIVIDENDO, DIVISOR Y RESTO
+	let dividendo, divisor, resto;
+
+	// TOMAMOS LOS DATOS DEL DIVIDENDO Y EL DIVISIOR POR ID
+	dividendo = document.getElementById("txtIdNumeroDividendo").value;
+	divisor = document.getElementById("txtIdNumeroDivisor").value;
+
+	// PARSEAMOS EL DIVIDENDO Y EL DIVISOR Y EL RESTO DE LA OPERACION LO ASIGNAMOS A LA VARIABLE RESTO
+	resto = parseInt(dividendo) % parseInt(divisor);
+
+	// MOSTRAMOS EN UN ALERT EL RESTO DE LA DIVISION
+	alert("El resto entre el dividendo y el divisor es " + resto);
 }
