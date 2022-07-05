9b2fd95bc8e2e53032e7dd32f013ed57ef6f3d1e
Leandro <leandrojvazquez97@gmail.com>
2022-07-05 12:24:50 -0300

    Se realizo el ejercicio 10

--- 1-EntradaSalida/jsEntradaSalida-09.js
+++ 1-EntradaSalida/jsEntradaSalida-09.js
@@ -1,10 +1,28 @@
 /*
+	VAZQUEZ, LEANDRO JAVIER
+	DIV E
+	EJERCICIO 09 E/S
 Debemos lograr tomar el importe por ID ,
 transformarlo a entero (parseInt), luego
 mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
+	// DECLARAMOS LAS VARIABLES PARA DISCRIMINAR EL SUELDO, EL AUMENTO Y EL RESULTADO FINAL
+	let sueldo, aumento, sueldoAumentado;
 
-	alert("ok");
+	// TOMAMOS EL SUELDO POR ID
+	sueldo = document.getElementById("txtIdSueldo").value;
+
+	// PARSEAMOS EL SUELDO
+	sueldo = parseInt(sueldo);
+
+	// CALCULAMOS EL AUMENTOO
+	aumento = (10/100) * sueldo;
+
+	// CALCULAMOS EL SUELDO MAS EL AUMENTO
+	sueldoAumentado = sueldo + aumento;
+
+	// MOSTRAMOS LOS RESULTADOS DE LAS VARIABLES EN UN ID
+	alert("El sueldo es " + sueldo + ", el aumento es de " + aumento + " y el sueldo quedaria en " + sueldoAumentado);
 }
--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -1,9 +1,30 @@
 /*
+	VAZQUEZ, LEANDRO JAVIER
+	DIV E
+	EJERCICIO 10 E/S
+
 Debemos lograr tomar el importe por ID.
 Transformarlo a entero (parseInt), luego
 mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	alert("ok");
+	// DECLARAMOS VARIABLES PARA EL IMPORTE, EL MONTO DEL DESCUENTO Y EL RESULTADO
+	let importe, descuento, resultado;
+
+	// PEDIMOS EL IMPORTE POR ID
+	importe = document.getElementById("txtIdImporte").value;
+
+	// PARSEAMOS EL IMPORTE 
+	importe = parseInt(importe);
+
+	// CALCULAMOS EL 25% DE DESCUENTO
+	descuento = (25/100) * importe;
+
+	// AL IMPORTE LE RESTAMOS EL DESCUENTO
+	resultado = importe - descuento;
+
+	// MOSTRAMOS POR ALERT EL DESCUENTO Y POR EL ID DE RESULTADO
+	//alert("El importe con el descuento del 25% es de " + resultado);
+	document.getElementById("txtIdResultado").value = resultado;
 }
