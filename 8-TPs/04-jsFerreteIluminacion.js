8b309b7b6037d3157b3df38280d3d8b72b9ee828
Leandro <leandrojvazquez97@gmail.com>
2022-07-14 11:45:53 -0300

    Se realizo el parcial 04(2018) y el tp 04

--- 8-TPs/04-jsFerreteIluminacion.js
+++ 8-TPs/04-jsFerreteIluminacion.js
@@ -8,7 +8,139 @@ E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de 
  ”Usted pago X de IIBB.”, siendo X el impuesto que se pagó. 
 
  */
+
+/*
+    VAZQUEZ, LEANDRO JAVIER
+    DIV E
+    TP 04, PARTE E
+*/
+
 function CalcularPrecio () 
 {
- 	
+    // SE CREA 4 VARIABLES PARA HACER MAS PROLIJO EL CODIGO
+ 	let cantidadLamparitas, precio , precioFinal, descuento, marca;
+
+    // LA VARIABLE PRECIO SE LE ASIGNA EL VALOR DE LAS LAMPARITAS
+    precio = 35;
+
+    marca = document.getElementById("Marca").value;
+
+    // SE PIDE LA CANTIDAD DE LAMPARITAS A COMPRAR
+    cantidadLamparitas = document.getElementById("txtIdCantidad").value;
+
+    // SE PARSEA LA CANTIDAD
+    cantidadLamparitas = parseInt(cantidadLamparitas);
+
+    // SI LA CANTIDAD A COMPRAR ES MAYOR O IGUAL A 6, SE CALCULA EL PRECIO CON UN DESCUENTO DEL 50%
+    // Y SE MUESTRA POR ID EL PRECIO FINAL CON EL DESCUENTO
+    if(6 <= cantidadLamparitas)
+    {
+        descuento = precio * (50/100);
+
+        precioFinal = (precio - descuento) * cantidadLamparitas;
+
+        // document.getElementById("txtIdprecioDescuento").value = precioFinal;
+    }
+
+    // SE CREA UN ELSE IF PARA CUANDO LA CANTIDAD DE LAMPARITAS SEA 5 
+    // UN IF ADENTRO PARA ASIGNAR SU RESPECTIVO DESCUENTO DEPENDIENDO SI SU MARCA ES "ARGENTINALUZ"
+    // UN ELSE PARA ASIGNAR SU RESPECTIVO DESCUENTO SI SU MARCA NO ES "ARGENTINALUZ"
+    else if(cantidadLamparitas == 5)
+    {
+        if(marca == "ArgentinaLuz")
+        {
+            descuento = precio * (40/100);
+
+            precioFinal = (precio - descuento) * cantidadLamparitas;
+
+            //document.getElementById("txtIdprecioDescuento").value = precioFinal;
+        }
+        else
+        {
+            descuento = precio * (30/100);
+
+            precioFinal = (precio - descuento) * cantidadLamparitas;
+
+            //document.getElementById("txtIdprecioDescuento").value = precioFinal;
+        }
+    }
+
+    // SE CREA OTRO IF ELSE SI LA CANTIDAD DE LAMPARITAS ES 4
+    // UN IF ADENTRO PARA ASIGNAR SI LA MARCA ES "ARGENTINALUZ" O "FELIPELAMPARAS" SE DESCUENTA 25% 
+    // UN ELSE SI ES DE OTRA MARCA SE LE DESCUENTA 20%
+    else if(cantidadLamparitas == 4)
+    {
+        if(marca == "ArgentinaLuz" || marca == "FelipeLamparas")
+        {
+            descuento = precio * (25/100);
+
+            precioFinal = (precio - descuento) * cantidadLamparitas;
+
+            //document.getElementById("txtIdprecioDescuento").value = precioFinal;
+        }
+        else
+        {
+            descuento = precio * (20/100);
+
+            precioFinal = (precio - descuento) * cantidadLamparitas;
+
+            //document.getElementById("txtIdprecioDescuento").value = precioFinal;
+        }
+    }
+
+    // SE CREA OTRO IF ELSE SI LA CANTIDAD DE LAMPARITAS ES 3
+    // UN IF DENTRO CON 15% DE DESCUENTO SI LA MARCA ES "ARGENTINALUZ"
+    // UN ELSE IF DENTRO CON 10% DE DESCUENTO SI LA MARCA ES "FELIPELAMPARAS"
+    // UN ELSE CON 5% DE DESCUENTO SI ES OTRA MARCA 
+    else if(cantidadLamparitas == 3)
+    {
+        if(marca == "ArgentinaLuz")
+        {
+            descuento = precio * (15/100);
+
+            precioFinal = (precio - descuento) * cantidadLamparitas;
+
+            //document.getElementById("txtIdprecioDescuento").value = precioFinal;
+        }
+        else if(marca == "FelipeLamparas")
+        {
+            descuento = precio * (10/100);
+
+            precioFinal = (precio - descuento) * cantidadLamparitas;
+
+            //document.getElementById("txtIdprecioDescuento").value = precioFinal;
+        }
+        else
+        {
+            descuento = precio * (5/100);
+
+            precioFinal = (precio - descuento) * cantidadLamparitas;
+
+            //document.getElementById("txtIdprecioDescuento").value = precioFinal;
+        }
+    }
+    
+    // SE CREA UN ELSE POR SI LA CANTIDAD DE LAMPARITAS NO ENTRA EN LOS DESCUENTOS
+    else
+    {
+        precioFinal = precio * cantidadLamparitas;
+
+        //document.getElementById("txtIdprecioDescuento").value = precioFinal;
+    }
+
+    // SE CREA UN IF PARA RECIBIR EL PRECIO FINAL DE LAS ANTERIORES CONDICIONES
+    // SI PRECIO FINAL ES MAYOR A $120 SE LE COBRA UN IMPUESTO DE 10%
+    if(120 < precioFinal)
+    {
+        let impuesto;
+
+        impuesto = precioFinal * (10/100);
+        
+        precioFinal = precioFinal + impuesto;
+
+        alert("”Usted pago " + precioFinal + " con el impuesto de IIBB., siendo " + impuesto + " el impuesto que se pagó");
+    }
+
+    // SE ASIGNA EL PRECIO FINAL AL ID DESPUES DE PASAR POR EL PROCESO DE DESCUENTO Y DE IMPUESTOS
+    document.getElementById("txtIdprecioDescuento").value = precioFinal;
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
@@ -1,4 +1,45 @@
+/*
+    VAZQUEZ, LEANDRO JAVIER
+    DIV E 
+    PARCIAL 04 , IF, (2018)
+*/
+
 function mostrar()
 {
+    // CREAMOS VARIABLE PARA LOS DOS VALORES Y EL RESULTADO
+    let numero1, numero2, resultado;
 
+    // SE PIDEN LOS DOS VALORES
+    numero1 = prompt("Ingrese un numero");
+    numero2 = prompt("Ingrese otro numero");
+
+    // SE PARSEAN LOS VALORES PEDIDOS
+    numero1 = parseInt(numero1);
+    numero2 = parseInt(numero2);
+    
+    // SI VALOR1 Y VALOR2 SON IGUALES SE ENVIA UN ALERT
+    if(numero1 == numero2)
+    {
+        alert("Los numeros son iguales");
+    }
+    
+    // SI EL VALOR1 ES MAYOR QUE VALOR2, SE RESTAN
+    else if(numero1 > numero2)
+    {
+        resultado = numero1 - numero2;
+
+        alert("El valor numero 1 es mayor que el numero 2 y el resultado de la resta es " + resultado);
+    }
+
+    // SI VALOR2 ES MAYOR A VALOR1 SE SUMAN LOS RESULTADOS
+    // SI EL VALOR DE LA SUMA ES MAS DE 10 SE ENVIA EL ALERT
+    else
+    {
+        resultado = numero1 + numero2;
+
+        if(resultado > 10)
+        {
+            alert("El resultado es " + resultado + " y supero el 10");
+        }
+    }
 }
